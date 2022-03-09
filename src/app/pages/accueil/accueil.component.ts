63bb4c77b2267318577d832eb2247ab69510b721
mickael2801 <90626204+mickael2801@users.noreply.github.com>
2022-03-09 15:58:06 +0100

    commit

--- src/app/pages/accueil/accueil.component.ts
+++ src/app/pages/accueil/accueil.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable, Observer} from 'rxjs';
+import { nodeModuleNameResolver } from 'typescript';
 
 export interface ExampleTab {
   graph: object;
@@ -11,78 +12,23 @@ export interface ExampleTab {
   styleUrls: ['./accueil.component.css']
 })
 export class AccueilComponent implements OnInit {
-  
+  options: any;
+  options2 : any;
+  constructor() {}
+
   ngOnInit(): void {
+    const xAxisData = ["Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Decembre"];
+    const data1 = [100];
+    const data2 = [100];
 
-    // const xAxisData = ['janvier','fevrier'];
-    // const chiffreaffaire = [100,200];
-    // const profit = [80,80];
-
-    // // for (let i = 0; i < 100; i++) {
-    // //   xAxisData.push('category' + i);
-    // //   data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
-    // //   data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
-    // // }
-
-    // this.options = {
-    //   legend: {
-    //     data: ['bar', 'bar2'],
-    //     align: 'left',
-    //   },
-    //   tooltip: {},
-    //   xAxis: {
-    //     data: xAxisData,
-    //     silent: false,
-    //     splitLine: {
-    //       show: false,
-    //     },
-    //   },
-    //   yAxis: {},
-    //   series: [
-    //     {
-    //       name: 'bar',
-    //       type: 'bar',
-    //       data: chiffreaffaire,
-    //       animationDelay: (idx: number) => idx * 10,
-    //     },
-    //     {
-    //       name: 'bar2',
-    //       type: 'bar',
-    //       data: profit,
-    //       animationDelay: (idx: number) => idx * 10 + 100,
-    //     },
-    //   ],
-    //   animationEasing: 'elasticOut',
-    //   animationDelayUpdate: (idx: number) => idx * 5,
-    // };
-  }
-  // options: any;
-  asyncTabs: Observable<ExampleTab[]>;
-
-  constructor() {
-    this.asyncTabs = new Observable((observer: Observer<ExampleTab[]>) => {
-      setTimeout(() => {
-        observer.next([
-          {graph : this.options},
-          {graph : this.options}
-          
-        ]);
-      }, 1000);
-    });
-  }
- 
-    // const xAxisData = ['janvier','fevrier'];
-    // const chiffreaffaire = [100,200];
-    // const profit = [80,80];
-
-      options:any = {
+    this.options = {
       legend: {
-        data: ['bar', 'bar2'],
+        data: ["chiffre d'affaires", 'benefices'],
         align: 'left',
       },
       tooltip: {},
       xAxis: {
-        data: ['janvier','fevrier'],
+        data: xAxisData,
         silent: false,
         splitLine: {
           show: false,
@@ -91,23 +37,52 @@ export class AccueilComponent implements OnInit {
       yAxis: {},
       series: [
         {
-          name: 'bar',
+          name: "chiffre d'affaires",
           type: 'bar',
-          data: [100,200],
+          data: data1,
           animationDelay: (idx: number) => idx * 10,
         },
         {
-          name: 'bar2',
+          name: 'benefices',
           type: 'bar',
-          data: [80,80],
+          data: data2,
           animationDelay: (idx: number) => idx * 10 + 100,
         },
       ],
       animationEasing: 'elasticOut',
       animationDelayUpdate: (idx: number) => idx * 5,
     };
-  
-
-  
+    this.options2 = {
+      legend: {
+        data: ['en stock', 'vendus'],
+        align: 'left',
+      },
+      tooltip: {},
+      xAxis: {
+        data: xAxisData,
+        silent: false,
+        splitLine: {
+          show: false,
+        },
+      },
+      yAxis: {},
+      series: [
+        {
+          name: 'en stock',
+          type: 'bar',
+          data: data1,
+          animationDelay: (idx: number) => idx * 10,
+        },
+        {
+          name: 'vendus',
+          type: 'bar',
+          data: data2,
+          animationDelay: (idx: number) => idx * 10 + 100,
+        },
+      ],
+      animationEasing: 'elasticOut',
+      animationDelayUpdate: (idx: number) => idx * 5,
+    };
+  }
 
 }
