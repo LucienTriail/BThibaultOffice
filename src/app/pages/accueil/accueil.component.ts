b4a79b25ad305e92a3b96b131b61d5ef4ef91a64
LucienTriail <lucien.triail@gmail.com>
2022-03-09 19:29:53 +0100

    changement du graphe d'accueil (de barres à ligne), et ajout d'une option pour l'update

--- src/app/pages/accueil/accueil.component.ts
+++ src/app/pages/accueil/accueil.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {Observable, Observer} from 'rxjs';
-import { nodeModuleNameResolver } from 'typescript';
+import { Component, OnInit , OnDestroy } from '@angular/core';
+
 
 export interface ExampleTab {
   graph: object;
@@ -12,12 +11,30 @@ export interface ExampleTab {
   styleUrls: ['./accueil.component.css']
 })
 export class AccueilComponent implements OnInit {
-  options: any;
-  options2 : any;
+  revenue :number[] = [];
+   profit:number[] = []
+  options:any;
+  updateOptions: any;
+
+  //TODO: faire call API au lieu des randomData...
+randomData(){
+  for (let i=0;i<12;i++){
+    let truc = Math.random();
+    this.profit.push(truc)
+  }
+
+  for (let j=0;j<12;j++){
+    let truc = Math.random();
+    this.revenue.push(truc)
+  }
+}
+
+
   constructor() {}
 
   ngOnInit(): void {
-    const xAxisData = ["Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Decembre"];
+  this.randomData();
+    /*const xAxisData = ["Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Decembre"];
     const data1 = [100];
     const data2 = [100];
 
@@ -84,5 +101,77 @@ export class AccueilComponent implements OnInit {
       animationDelayUpdate: (idx: number) => idx * 5,
     };
   }
+*/
+
+
+
+
+      this.updateOptions = {
+        series: [{
+          data: this.revenue,
+          data2:this.profit
+        }]
+      };
+
+
+    this.options = {
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross',
+          label: {
+            backgroundColor: '#6a7985'
+          }
+        }
+      },
+      legend: {
+        data: ["Chiffre d'affaire", "Bénéfices"]
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          boundaryGap: false,
+          data: ["Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Decembre"]
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: "Chiffre d'affaire",
+          type: 'line',
+          stack: 'counts',
+          areaStyle: {},
+          data: this.revenue
+        },
+        {
+          name: 'Bénéfices',
+          type: 'line',
+          stack: 'counts',
+            areaStyle: {},
+            data: this.profit
+
+        }
+
+
+      ]
+    };
+
+
+  }
+
+
+
+
 
 }
+
