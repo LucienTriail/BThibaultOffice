290fb391719ca2b33e7aa4617a70dd3fffcffb1c
LucienTriail <lucien.triail@gmail.com>
2022-03-14 08:51:45 +0100

    ajout des méthodes d'init chart dans accueil et suppression de home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,20 @@
 import {ErrorHandler, NgModule} from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
 import {ApiService} from "./core/services/api.service";
 import {AppRoutingModule} from "./app-routing.module";
-import { HomeComponent } from './pages/home/home.component';
+import {HomeComponent} from './pages/home/home.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatIconModule} from "@angular/material/icon";
-import {MatGridListModule} from "@angular/material/grid-list";
-import {MatButtonModule} from "@angular/material/button";
-import {MatTableModule} from "@angular/material/table";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {DialogSingleProductModule} from "./shared/dialog-single-product/dialog-single-product.module";
 import {SingleProductCardModule} from "./shared/single-product-card/single-product-card.module";
 import {HeaderModule} from "./shared/header/header.module";
 import {BasicButtonModule} from "./shared/buttons/basic-button/basic-button.module";
 import {DetailsProductModule} from "./pages/details-product/details-product.module";
 import {FooterModule} from "./shared/footer/footer.module";
-import { LoginModule } from './pages/login/login.module';
-import { AccueilModule } from './pages/accueil/accueil.module';
-import { JwtModule } from "@auth0/angular-jwt";
+import {LoginModule} from './pages/login/login.module';
+import {AccueilModule} from './pages/accueil/accueil.module';
+import {JwtModule} from "@auth0/angular-jwt";
 import {GlobalHttpInterceptorService} from "./core/services/global-http-interceptor.service";
 import {GlobalErrorHandlerService} from "./core/services/global-error-handler.service";
 import {ErrorModule} from "./pages/error/error.module";
@@ -28,8 +23,6 @@ import {UserDetailModule} from "./pages/user-detail/user-detail.module";
 export function tokenGetter() {
   return localStorage.getItem("access_token");
 }
-
-
 
 
 @NgModule({
@@ -42,11 +35,6 @@ export function tokenGetter() {
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatGridListModule,
-    MatButtonModule,
-    MatTableModule,
     DialogSingleProductModule,
     SingleProductCardModule,
     HeaderModule,
@@ -69,10 +57,11 @@ export function tokenGetter() {
 
   providers: [
     ApiService,
-    { provide: HTTP_INTERCEPTORS, useClass: GlobalHttpInterceptorService, multi: true  },
-    { provide: ErrorHandler, useClass:GlobalErrorHandlerService}
+    {provide: HTTP_INTERCEPTORS, useClass: GlobalHttpInterceptorService, multi: true},
+    {provide: ErrorHandler, useClass: GlobalErrorHandlerService}
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
 
