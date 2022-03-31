532abbd5151b2c76edaf3b41d8494ecaaf0ac24e
issouf-sacko <issoufsacko1@gmail.com>
2022-03-31 10:56:31 +0200

    fix filter V2

--- src/app/pages/stock-product/stock-product.component.ts
+++ src/app/pages/stock-product/stock-product.component.ts
@@ -25,7 +25,7 @@ export class StockProductComponent implements OnInit, AfterViewInit {
   // @ts-ignore
   dataSource: MatTableDataSource<StockTransac> ;
   empFilters: EmpFilter[]=[];
-  category: string[]=['All','poissons','crustacé'];
+  category: string[]=['All','poissons','crustacés'];
   defaultValue = '';
   operations = [
     {value: ''},
@@ -61,7 +61,6 @@ export class StockProductComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.loadData();
     // @ts-ignore
     this.dataSource.paginator = this.paginator;
     // @ts-ignore
@@ -156,206 +155,7 @@ export class StockProductComponent implements OnInit, AfterViewInit {
 
   }
 
- async loadData() {
-    this.lstStockTransac  = await this.lData();
+ loadData() {
+    this.lstStockTransac  = this.lData();
   }
 }
-
-// @ts-ignore
-const products: Products[] = [{
-  "comments": "",
-  "category": "poissons",
-  "availability": true,
-  "id": 6,
-  "price": 7,
-  "discounted_price": 6,
-  "discount": 5,
-  "sale": true,
-  "owner": "tig",
-  "unit": "kg",
-  "name": "Moules de pêche",
-  "stock": 20,
-  "sold": 50
-},
-  {
-    "id": 1,
-    "unit": "2 filets",
-    "category": "poissons",
-    "name": "Filet Bar de ligne",
-    "discount": 0,
-    "comments": "environ 300g",
-    "owner": "tig",
-    "price": 7,
-    "discounted_price": 7,
-    "sale": false,
-    "availability": true,
-    "stock": 60,
-    "sold": 100
-  },
-  {
-    "availability": false,
-    "price": 7,
-    "discounted_price": 7,
-    "discount": 0,
-    "name": "Araignées",
-    "comments": "Hors saison, pêche aux casiers",
-    "id": 9,
-    "owner": "tig",
-    "unit": "kg",
-    "category": "poissonss",
-    "sale": false,
-    "stock": 30,
-    "sold": 10
-  },
-  {
-    "name": "Bouquets cuits",
-    "sale": false,
-    "id": 10,
-    "category": "crustascé",
-    "price": 8,
-    "discounted_price": 8,
-    "availability": false,
-    "unit": "100g",
-    "owner": "tig",
-    "comments": "Hors saison, pêche à l'épuisette",
-    "discount": 0,
-    "stock": 40,
-    "sold": 60
-  },
-  {
-    "unit": "Dz",
-    "comments": "",
-    "availability": true,
-    "category": "crustascé",
-    "id": 7,
-    "name": "Huitres N°2 St Vaast",
-    "price": 9.5,
-    "discounted_price": 9.5,
-    "sale": false,
-    "owner": "tig",
-    "discount": 0,
-    "stock": 20,
-    "sold": 80
-  },
-  {
-    "owner": "tig",
-    "id": 2,
-    "name": "Bar de ligne portion",
-    "category": "crustascé",
-    "unit": "pièce",
-    "comments": "Environ 550-700g la pièce",
-    "sale": false,
-    "price": 10,
-    "discounted_price": 10,
-    "discount": 0,
-    "availability": true,
-    "stock": 10,
-    "sold": 100
-  },
-  {
-    "comments": "Pêche à la corde",
-    "category": "crustascé",
-    "discount": 0,
-    "id": 12,
-    "name": "Aile de raie",
-    "sale": false,
-    "availability": true,
-    "owner": "tig",
-    "price": 10,
-    "discounted_price": 10,
-    "unit": "kg",
-    "stock": 20,
-    "sold": 20
-  },
-  {
-    "unit": "Dz",
-    "sale": false,
-    "price": 12,
-    "discounted_price": 12,
-    "availability": true,
-    "id": 13,
-    "name": "Huîtres N°2 OR St Vaast",
-    "discount": 0,
-    "comments": "Médaille d'or Salon Agriculture",
-    "category": "crustascé",
-    "owner": "tig",
-    "stock": 20,
-    "sold": 100
-  },
-  {
-    "name": "Lieu jaune de ligne",
-    "availability": true,
-    "unit": "kg",
-    "sale": false,
-    "category": "",
-    "owner": "tig",
-    "discount": 0,
-    "comments": "Environ 550-700g la portion",
-    "id": 4,
-    "price": 12,
-    "discounted_price": 12,
-    "stock": 30,
-    "sold": 80
-  },
-  {
-    "availability": false,
-    "id": 5,
-    "unit": "kg",
-    "owner": "tig",
-    "discount": 0,
-    "price": 19,
-    "discounted_price": 19,
-    "category": "",
-    "name": "Filet Julienne",
-    "sale": false,
-    "comments": "Pêche à la corde",
-    "stock": 10,
-    "sold": 70
-  },
-  {
-    "sale": false,
-    "category": "crustascé",
-    "price": 19,
-    "discounted_price": 19,
-    "id": 16,
-    "unit": "2 Dz",
-    "discount": 0,
-    "name": "Huîtres N°2 St Vaast",
-    "owner": "tig",
-    "availability": true,
-    "comments": "",
-    "stock": 20,
-    "sold": 100
-  },
-  {
-    "owner": "tig",
-    "sale": false,
-    "availability": true,
-    "category": "crustascé",
-    "unit": "2 Dz",
-    "discount": 0,
-    "id": 14,
-    "comments": "Médaille d'or salon agriculture",
-    "price": 24,
-    "discounted_price": 24,
-    "name": "Huîtres N°2 OR St Vaast",
-    "stock": 20,
-    "sold": 100
-  },
-  {
-    "discount": 0,
-    "availability": true,
-    "owner": "tig",
-    "name": "Bar de ligne",
-    "category": "",
-    "sale": false,
-    "comments": "Plus de 1.5kg le poisson",
-    "id": 3,
-    "unit": "kg",
-    "price": 30,
-    "discounted_price": 30,
-    "stock": 10,
-    "sold": 100
-  }];
-
-
